7d772cec0bb5c5c009f5499c0873ea3c627055c4
[아이엔소프트] 황호세 <rhwkd5012@naver.com>
2023-02-14 10:36:52 +0900

    feat: Chatting에 필요한 상자 구성

--- src/components/ChatBox.tsx
+++ src/components/ChatBox.tsx
@@ -1,17 +1,35 @@
+import { useEffect, useState } from 'react';
 import styled from 'styled-components';
+import { socket } from '../apis/socketio';
 import { Centering } from '../styles/Centering';
+import MyChat from './chat/MyChat';
+import OtherChat from './chat/OtherChat';
+
+type chatType = {
+  userId: string;
+  chat: string
+}
 
 const ChatBox = () => {
+  const [ chatArr, setChatArr ] = useState<chatType[]>([]);
+  useEffect(() => {
+    socket.on('msg', (data: chatType) => {
+      chatArr.push(data);
+      setChatArr(chatArr)
+    });
+  }, []);
   return (
     <ChatBoxContainer>
-      
+      {
+        chatArr.map(chat => chat.userId === )
+      }
     </ChatBoxContainer>
   );
 };
 
 const ChatBoxContainer = styled(Centering)`
-width: 90vw;
-height: 90vh;
+width: 800px;
+height: 80vh;
 border-radius: 20px;
 border: 2px solid black;
 `;
